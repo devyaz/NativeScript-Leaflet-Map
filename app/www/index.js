ce8b3425c9389e7398b30cfca42158765fe39bef
devyaz <mtunthama@gmail.com>
2018-06-28 15:46:51 +0200

    Update index.js

--- app/www/index.js
+++ app/www/index.js
@@ -1,17 +1,6 @@
 (function () {
     var oWebViewInterface = window.nsWebViewInterface;
-  //  var languageDD = document.getElementById('knownLanguage');
-
-    /**
-     * Registers handlers for native events.
-     */
-    /*function addNativeMsgListener() {
-        oWebViewInterface.on('loadLanguages', function (arrLanguages) {
-            for (var i = 0; i < arrLanguages.length; i++) {
-                addLanguageOption(arrLanguages[i]);
-            }
-        });*/
-
+ 
  oWebViewInterface.on('marker', function (ltlng) {
     var data=ltlng.toString();
     var rmv=data.split('LatLng(');
@@ -21,20 +10,7 @@
     var lat=output[0];
     var lang=output[1];
     console.log("Data from Home page =>"+lat+" and  long >"+lang);
- /*var myIcon = L.icon({
-    iconUrl: '../css/images/green_pin_marker.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: '../css/images/marker-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-    });
-var DynaMarker = L.marker([lat,lang], {icon: myIcon} )
-    .addTo(map).bindPopup('NEW SHINY<br> MARKER')
-    .openPopup();
-    map.flyTo([lat, lang], 14,'animate');
- // });*/
+
 });
 
     /**
